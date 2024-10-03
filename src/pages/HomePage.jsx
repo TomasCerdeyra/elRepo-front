0070762c34b2bc6b799eb796b435fea2784b2200
matilopezz <matilopezo25@gmail.com>
2024-10-03 10:06:38 -0300

    Redimensiones (Login y Home) y Head

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { getCarreras } from '../services/carrerasServices';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import logos from '../services/logosServices';
@@ -23,41 +23,39 @@ const HomePage = () => {
   useEffect(() => {
     const cargarCarreras = async () => {
       try {
-        const data = await getCarreras()
-        setCarreras(agruparCarrerasPorArea(data))
+        const data = await getCarreras();
+        setCarreras(agruparCarrerasPorArea(data));
       } catch (error) {
         console.log('Error al cargar las carreras');
       }
-    }
+    };
     cargarCarreras();
   }, []);
 
   return (
-    <div className='bg-[#EFF3F5]'>
+    <div className='min-h-screen flex flex-col bg-[#EFF3F5]'>
       <Navbar />
-      <div className='py-4'>
-        <h1 className=' text-[#16353B] text-2xl text-center font-bold font-sans'>ELIGE TU CARRERA UNIVERSITARIA:</h1>
+      <div className='py-4 flex-grow'>
+        <h1 className='text-[#16353B] text-2xl md:text-3xl lg:text-4xl xl:text-5xl text-center font-bold font-sans'>
+          ELIGE TU CARRERA UNIVERSITARIA:
+        </h1>
         {Object.keys(carreras).map((area) => (
-          <div className=' text-white text-1xl text-center font-bold font-sans px-5' key={area}>
-            <div className=" h-[.13rem] mt-7 mb-5 bg-[#4F847C]"></div>
+          <div className='text-white text-1xl md:text-2xl lg:text-3xl xl:text-4xl text-center font-bold font-sans px-5' key={area}>
+            <div className="h-[.13rem] mt-7 mb-5 bg-[#4F847C]"></div>
             <div className='flex flex-col items-center'>
-              <img src={logos[area]} class="h-9 object-contain" alt="d" />
+              <img src={logos[area]} className="h-9 md:h-12 lg:h-16 xl:h-20 object-contain" alt={area} />
             </div>
-            <h2 className='text-[#16353B] text-xl mb-5'>{area}</h2> {/* Título del área */}
-
+            <h2 className='text-[#16353B] text-xl md:text-2xl lg:text-3xl xl:text-4xl mb-5'>{area}</h2> {/* Título del área */}
             <ul className='flex flex-col gap-2 px-6'>
               {carreras[area].map((carrera) => (
-                <div className='bg-[#4F847C] font-light px-3' key={carrera._id}>
+                <div className='bg-[#4F847C] font-light px-3 md:px-4 lg:px-5 xl:px-6' key={carrera._id}>
                   <Link to={`/carrera/${carrera._id}`}>
                     {carrera.name}
                   </Link>
-
                 </div>
               ))}
             </ul>
-
           </div>
-
         ))}
       </div>
       <Footer />
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -10,16 +10,19 @@ const LoginPage = () => {
   }
 
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
       <Navbar/>
 
-      <h1 className='text-[#16353B] text-2xl text-center font-bold font-sans mt-8'>INICIAR SESIÓN</h1>
-      <div className=" h-1 bg-[#4F847C] mt-4 mx-9 mb-6"></div>
-      <div class="flex justify-center">
-      <button className='mt-4 rounded text-white bg-red-600 hover:bg-red-700' onClick={handleLogin}>
-        Iniciar Sesion con Gogle
-      </button>
+      <h1 className='text-[#16353B] text-2xl md:text-3xl lg:text-4xl xl:text-5xl text-center font-bold font-sans mt-8'>
+        INICIAR SESIÓN
+      </h1>
+      <div className="h-1 bg-[#4F847C] mt-4 mx-auto mb-6 w-4/5 md:w-3/5 lg:w-1/2 xl:w-2/5"></div>
+      <div className="flex justify-center">
+        <button className='mt-4 rounded text-white bg-red-600 hover:bg-red-700 p-2 md:p-4 lg:p-6 xl:p-8' onClick={handleLogin}>
+          Iniciar Sesion con Google
+        </button>
       </div>
+      <Footer />
     </div>
   )
 }
