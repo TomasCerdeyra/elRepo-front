f9cf91d9c4c5d9dbf15ec58518d267adb1a03d64
Franco <leszkiewiczfranco@gmail.com>
2024-10-03 14:28:20 -0300

    Button carreras css

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -41,18 +41,20 @@ const HomePage = () => {
         </h1>
         {Object.keys(carreras).map((area) => (
           <div className='text-white text-1xl md:text-2xl lg:text-3xl xl:text-4xl text-center font-bold font-sans px-5' key={area}>
-            <div className="h-[.13rem] mt-7 mb-5 bg-[#4F847C]"></div>
+            <div className=" h-1 bg-[#4F847C] mt-8 mx-9 mb-6"></div>
             <div className='flex flex-col items-center'>
               <img src={logos[area]} className="h-9 md:h-12 lg:h-16 xl:h-20 object-contain" alt={area} />
             </div>
             <h2 className='text-[#16353B] text-xl md:text-2xl lg:text-3xl xl:text-4xl mb-5'>{area}</h2> {/* Título del área */}
             <ul className='flex flex-col gap-2 px-6'>
               {carreras[area].map((carrera) => (
-                <div className='bg-[#4F847C] font-light py-2 md:py-3 lg:py-4 xl:py-5 px-3 md:px-4 lg:px-5 xl:px-6 w-full md:w-3/4 lg:w-1/2 xl:w-1/3 mx-auto flex items-center justify-center' key={carrera._id}>
-                  <Link to={`/carrera/${carrera._id}`} className='text-sm md:text-base lg:text-lg xl:text-xl text-center'>
-                    {carrera.name}
-                  </Link>
+                
+                <Link to={`/carrera/${carrera._id}`} className='w-full md:w-3/4 lg:w-1/2 xl:w-1/3 mx-auto block text-sm md:text-base lg:text-lg xl:text-xl text-center'>
+                <div className='bg-[#4F847C] font-light py-2 md:py-3 lg:py-4 xl:py-5 px-3 md:px-4 lg:px-5 xl:px-6 flex items-center justify-center' key={carrera._id}>
+                  <p className="flex-1 text-white font-semibold">{carrera.name}</p>
                 </div>
+              </Link>
+                
               ))}
             </ul>
           </div>
