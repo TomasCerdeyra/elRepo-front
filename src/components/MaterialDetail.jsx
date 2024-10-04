03489baedc988d2826fab56b6c36de9b22f8578d
Tomas <tomascerdeyraimport@gmail.com>
2024-10-04 14:10:07 -0300

    feat: MaterialDetail imagen o archivo

--- src/components/MaterialDetail.jsx
+++ src/components/MaterialDetail.jsx
@@ -1,17 +1,26 @@
 import React from "react";
 
-const MaterialDetail = ({material}) => {
+const MaterialDetail = ({ material }) => {
 
-    const img = ` http://localhost:8080/${material.rutaArchivo}` 
-
-    return(
+    return (
         <div>
             <h2 className="uppercase text-[#16353B] text-2xl text-center my-5 font-bold font-sans">{material.nombre}</h2>
             <p>Año: {material.anio}</p>
             <p>Descripción: {material.descripcion}</p>
             <p>Profesor: {material.profesor}</p>
 
-            <img src={`http://localhost:8080/uploads/${material.rutaArchivo}`} alt={material.nombre} style={{ maxWidth: '100%' }} />
+            {material.tipo === 'imagen' && (
+                <img src={`http://localhost:8080/uploads/${material.rutaArchivo}`} alt={material.nombre} style={{ maxWidth: '100%' }} />
+            )}
+            {material.tipo === 'archivo' && (
+                <a href={`http://localhost:8080/uploads/${material.rutaArchivo}`} download target="_blank" rel="noopener noreferrer">
+                    <button className="bg-blue-500 text-white py-2 px-4 rounded">
+                        Descargar archivo
+                    </button>
+                </a>
+            )}
+
+
         </div>
     )
 }
