716f5cb55d5e4a48e4ee31c5eb81c61cbee9df73
Tomas <tomascerdeyraimport@gmail.com>
2024-10-29 20:13:15 -0300

    fix: materialesList logs

--- src/components/MaterialesList.jsx
+++ src/components/MaterialesList.jsx
@@ -1,13 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import logoImagen from '../assets/img.png';
 import logoArchivo from '../assets/archivo.png';
+import logoCarpeta from '../assets/carpeta.png'
+import logoCarpeta2 from '../assets/carpeta2.png'
 
 const MaterialesList = ({ materiales }) => {
 
-  useEffect(()=> {})
+  const seleccionarLogo = (tipoArchivos) => {
+    const tieneImagen = tipoArchivos.includes('imagen')
+    const tieneArchivo = tipoArchivos.includes('archivo')
 
+    if (tieneImagen && tieneArchivo) {
+      return { src: logoCarpeta, alt: 'Icono Carpeta' }
+    } else if (tieneImagen) {
+      return { src: logoImagen, alt: 'Icono Imagen' }
+    } else if (tieneArchivo) {
+      return { src: logoArchivo, alt: 'Icono Archivo' }
+    }
+  }
   return (
     <div className="mx-5 lg:mx-20 xl:mx-20">
       {materiales.length === 0 ? (
@@ -16,19 +28,22 @@ const MaterialesList = ({ materiales }) => {
         </p>
       ) : (
         <ul>
-          {materiales.map((material) => (
-            <div key={material._id} className="hover:bg-[#4F847C] hover:shadow-md transition duration-200 bg-[#ffff] m-2 flex items-center justify-between">
-              <Link to={`/material/${material._id}`} className="flex items-center justify-between w-full p-3 gap-3">
-                <img
-                  src={material.tipo === 'imagen' ? logoImagen : logoArchivo}
-                  alt={material.tipo === 'imagen' ? 'icono Imagen' : 'Icono Archivo'}
-                  className="h-9 sm:h-9 md:h-12 lg:h-16 xl:h-20 object-contain"
-                />
-                <p className="flex-1 font-semibold text-xl">{material.nombre}</p>
-                <p className="flex-1 text-center text-lg font-medium">Año: {material.anio}</p>
-              </Link>
-            </div>
-          ))}
+          {materiales.map((material) => {
+            const { src, alt } = seleccionarLogo(material.tipo)
+            return (
+              <div key={material._id} className="hover:bg-[#4F847C] hover:shadow-md transition duration-200 bg-[#ffff] m-2 flex items-center justify-between">
+                <Link to={`/material/${material._id}`} className="flex items-center justify-between w-full p-3 gap-3">
+                  <img
+                    src={src}
+                    alt={alt}
+                    className="h-9 sm:h-9 md:h-12 lg:h-16 xl:h-20 object-contain"
+                  />
+                  <p className="flex-1 font-semibold text-xl">{material.nombre}</p>
+                  <p className="flex-1 text-center text-lg font-medium">Año: {material.anio}</p>
+                </Link>
+              </div>
+            )
+          })}
         </ul>
       )}
     </div>
