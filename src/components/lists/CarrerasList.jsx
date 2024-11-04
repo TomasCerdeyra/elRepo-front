c7d543c90fdca389a3c1a71c857bbcdce374fb1e
Tomas <tomascerdeyraimport@gmail.com>
2024-11-04 17:29:03 -0300

    fix: login anda carrerasAdmin styles

--- src/components/lists/CarrerasList.jsx
+++ src/components/lists/CarrerasList.jsx
@@ -23,31 +23,34 @@ const CarrerasList = () => {
   const handleDelete = async (id) => {
 
     const respuesta = await showConfirmationAlert('¿Estás seguro de eliminar esta carrera?', 'Si la eliminas, no podrás recuperarla. ¿Deseas continuar?')
-      if (respuesta.isConfirmed) {
-        try {
-          const data = await deleteCarrera(id);
-          setCarreras(carreras.filter(carrera => carrera._id !== id));
+    if (respuesta.isConfirmed) {
+      try {
+        const data = await deleteCarrera(id);
+        setCarreras(carreras.filter(carrera => carrera._id !== id));
 
-          showSuccessAlert('¡Carrera Eliminada Correctamente!', 'success')
-        } catch (error) {
-          showSuccessAlert('Error', 'error', error.message)
-        }
+        showSuccessAlert('¡Carrera Eliminada Correctamente!', 'success')
+      } catch (error) {
+        showSuccessAlert('Error', 'error', error.message)
       }
+    }
   };
 
   return (
-    <ul className="mx-5 lg:mx-20 xl:mx-20">
+    <ul className="flex flex-col items-center px-5">
       {carreras.map((carrera) => (
-        <div key={carrera._id} className="hover:bg-[#4F847C] hover:shadow-md transition duration-200 bg-[#ffff] p-3 m-2 flex items-center justify-between gap-5">
-          <p className="flex-1 font-semibold text-2xl">{carrera.name}</p>
-          <Link to={`/admin/actualizarcarrera/${carrera._id}`} className="rounded text-xs font-medium ring ring-green-500 text-right bg-green-500 text-white mr-5">
-            Actualizar
-          </Link>
-          <button
-            className="rounded text-xs font-medium ring ring-red-500 text-right bg-red-500 text-white mr-5"
-            onClick={() => handleDelete(carrera._id)}>
-            Eliminar
-          </button>
+        <div key={carrera._id} className="hover:bg-[#4F847C] hover:shadow-md transition duration-200 bg-[#ffff] p-3 m-2 flex-col sm:flex-row flex items-center justify-between gap-5 w-full max-w-[1000px]">
+          <p className="flex-1 font-semibold text-2xl text-center sm:text-start">{carrera.name}</p>
+          <div className="flex gap-5">
+            <Link to={`/admin/actualizarcarrera/${carrera._id}`} className="p-1 rounded text-xs font-medium ring ring-green-500 text-right bg-green-500 text-white">
+              Actualizar
+            </Link>
+            <button
+              className="p-1 rounded text-xs font-medium ring ring-red-500 text-right bg-red-500 text-white"
+              onClick={() => handleDelete(carrera._id)}>
+              Eliminar
+            </button>
+          </div>
+
         </div>
       ))}
     </ul>
--- src/pages/principal/LoginPage.jsx
+++ src/pages/principal/LoginPage.jsx
@@ -11,32 +11,35 @@ const LoginPage = () => {
 
   return (
     <div className="min-h-screen flex flex-col">
-      <Navbar/>
+      <Navbar />
 
-      <div className="flex-grow">
-        <h1 className='text-[#16353B] text-2xl md:text-3xl lg:text-4xl xl:text-5xl text-center font-bold font-sans mt-8'>
-          INICIAR SESIÓN
-        </h1>
-        <div className="h-1 bg-[#4F847C] mt-4 mx-auto mb-6 w-4/5 md:w-3/5 lg:w-1/2 xl:w-2/5"></div>
+      <div className="flex-grow flex flex-col justify-center gap-10">
+        <div>
+          <h1 className='text-[#16353B] text-2xl md:text-3xl lg:text-4xl xl:text-5xl text-center font-bold font-sans mt-8'>
+            INICIAR SESIÓN
+          </h1>
+          <div className="h-[.15rem] bg-[#4F847C] mt-4 mx-auto w-4/5 md:w-3/5 lg:w-1/2 xl:w-2/5"></div>
+        </div>
+
         <div className="flex justify-center">
           <button className='flex items-center gap-2 w-[195px] h-[40px] min-w-[195px] px-[1px] pl-[10px] pr-[10px] rounded-[3px] border border-[rgba(0,0,0,0.10)] bg-[#DC4E41] text-white hover:bg-[#c54242] hover:shadow-md transition duration-200' onClick={handleLogin}>
             <img src="/public/google_icon.png" alt="Google Icon" className="h-6 w-6" />
             <span className="ml-2 leading-[1.1] font-medium">Loguearse con Google</span>
           </button>
+        </div>
 
+        <div className="flex justify-center items-center flex-col ">
+          <img className='w-7 h-auto opacity-70' src="/public/info.png" alt="info.png" />
+          <p className=" text-lg font-medium text-gray-400 max-w-7xl mx-auto px-4 pb4  text-center">
+            Nuestra plataforma está diseñada para brindar a los estudiantes de la Universidad Nacional de San Antonio de Areco (UNSADA) un espacio colaborativo y accesible, donde podrán encontrar todos los recursos que necesitan a lo largo de su carrera. Aquí tendrás acceso a una amplia colección de materiales aportados por otros estudiantes, como exámenes finales, parciales, trabajos prácticos y apuntes, organizados por carrera y materia.
+          </p>
+          <p className="text-center text-lg font-medium text-gray-400 mt-1 max-w-7xl mx-auto p-4">
+            Nuestro objetivo es acompañarte en tu trayecto académico y facilitarte el estudio mediante el intercambio de conocimientos y experiencias. Con todas las carreras de la UNSADA disponibles, encontrarás recursos específicos para cada materia, contribuyendo así al éxito de tu formación.
+          </p>
         </div>
-        <div className="flex justify-center mt-28">
-        <img className='w-7 h-auto opacity-70' src="/public/info.png" alt="info.png" />
-        </div>
-        <p className=" text-lg font-medium text-gray-400 mt-2 max-w-7xl mx-auto p-4 text-center">
-        Nuestra plataforma está diseñada para brindar a los estudiantes de la Universidad Nacional de San Antonio de Areco (UNSADA) un espacio colaborativo y accesible, donde podrán encontrar todos los recursos que necesitan a lo largo de su carrera. Aquí tendrás acceso a una amplia colección de materiales aportados por otros estudiantes, como exámenes finales, parciales, trabajos prácticos y apuntes, organizados por carrera y materia.
-        </p>
-        <p className="text-center text-lg font-medium text-gray-400 mt-1 max-w-7xl mx-auto p-4">
-        Nuestro objetivo es acompañarte en tu trayecto académico y facilitarte el estudio mediante el intercambio de conocimientos y experiencias. Con todas las carreras de la UNSADA disponibles, encontrarás recursos específicos para cada materia, contribuyendo así al éxito de tu formación.
-        </p>
-        
+
       </div>
-      
+
       <Footer />
     </div>
   )
